b16fe8b936352f691b36efc755afc5c6bbb93c7e
Alex92Rivas <alejandrojp1992@gmail.com>
2026-03-06 13:35:50 +0100

    fix vercel build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // ← activa el modo oscuro por clase
+  content: [
+    "./index.html",
+    "./app.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
